4aaaeb343b6a0d158051d527005af3c52f5dda8b
Renan Mello <mellorenan19@gmail.com>
2024-07-11 11:11:39 -0300

    axios fix 8.0

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -5,6 +5,7 @@ export default axios.create({
       headers:  {"Access-Control-Allow-Origin": "*",
                  "Access-Control-Allow-Credentials":"false",
                  "Access-Control-Allow-Methods": "*",
+                 "Access-Control-Max-Age":"1800",
                  "Access-Control-Allow-Headers":"X-CSRF-Token,X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" 
       }
 //        baseURL: 'https://9c96-103-106-239-104.ap.ngrok.io',
