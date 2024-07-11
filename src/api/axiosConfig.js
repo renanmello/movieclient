50973e241fa5e7aa047420bb38f464250f84f651
Renan Mello <mellorenan19@gmail.com>
2024-07-11 10:47:46 -0300

    axios fix 4.0

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 export default axios.create({
       baseURL: 'http://54.237.207.74:8080',
       headers:  {"ngrok-skip-browser-warning": "true",
-                 "Access-Control-Allow-Headers" : "Content-Type",
                  "Access-Control-Allow-Origin": "*",
-                 "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                 "Access-Control-Allow-Methods": "*",
+                 "Access-Control-Allow-Headers":"X-CSRF-Token,X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" 
       }
 //        baseURL: 'https://9c96-103-106-239-104.ap.ngrok.io',
 //        headers: {"ngrok-skip-browser-warning": "true"}
