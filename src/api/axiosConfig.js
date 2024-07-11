3053d45e256a3c0d48b93fd4324bb65f586b2622
Renan Mello <mellorenan19@gmail.com>
2024-07-11 10:21:24 -0300

    axios fix 3.0

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 
 export default axios.create({
       baseURL: 'http://54.237.207.74:8080',
-      headers:  {"ngrok-skip-browser-warning": "true"}
+      headers:  {"ngrok-skip-browser-warning": "true",
+                 "Access-Control-Allow-Headers" : "Content-Type",
+                 "Access-Control-Allow-Origin": "*",
+                 "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+      }
 //        baseURL: 'https://9c96-103-106-239-104.ap.ngrok.io',
 //        headers: {"ngrok-skip-browser-warning": "true"}
 });
